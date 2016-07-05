8ef26228b2015885494ceb50a26ba0e80c63d65f
M. S <whyponeros@gmail.com>
2016-07-05 23:41:38 +0200

    refactor(client): chunk face addition

---
+++ client/app/engine/graphics/graphics.sub.chunks.faces.js
@@ -0,0 +1,169 @@
+/**
+ *
+ */
+
+'use strict';
+
+App.Engine.Graphics.prototype.addFace = function(faceId, i, iS, ijS, ijkS,
+                                                 positions, normals, colors, natures,
+                                                 iChunkOffset, jChunkOffset,
+                                                 pA, pB, pC, cb, ab,
+                                                 normal, color, n) {
+
+    var j;
+    var ax, bx, cx, dx, ay, by, cy, dy, az, bz, cz, dz;
+    var ii, jj, kk;
+    var nx, ny, nz;
+
+    if (faceId < ijS) {
+        ii = faceId % iS;
+        jj = (faceId - ii) / iS;
+        kk = (faceId - ii - jj*iS) / ijS;
+
+        ax = iChunkOffset + 1 + ii; ay = jChunkOffset + jj; az = kk;
+        bx = ax; by = ay; bz = az + 1;
+        cx = ax; cy = ay + 1; cz = az + 1;
+        dx = ax; dy = ay + 1; dz = az;
+
+        // Positions H1
+        positions[i]   = ax; positions[i+1] = ay; positions[i+2] = az;
+        positions[i+3] = normal ? bx : cx; positions[i+4] = normal ? by : cy; positions[i+5] = normal ? bz : cz;
+        positions[i+6] = normal ? cx : bx; positions[i+7] = normal ? cy : by; positions[i+8] = normal ? cz : bz;
+
+        // Normals H1
+        pA.set(ax, ay, az);
+        normal ? pB.set(bx, by, bz) : pB.set(cx, cy, cz);
+        normal ? pC.set(cx, cy, cz) : pC.set(bx, by, bz);
+        cb.subVectors(pC, pB);
+        ab.subVectors(pA, pB);
+        cb.cross(ab);
+        cb.normalize();
+        nx = cb.x; ny = cb.y; nz = cb.z;
+        for (j = 0; j<3; ++j) {normals[i+3*j]   = nx; normals[i+3*j+1] = ny; normals[i+3*j+2] = nz;}
+
+        // Colors H1
+        color.setRGB((ax/n)+0.5, (ay/n)+0.5, (az/n)+0.5);
+        for (j = 0; j<3; ++j) {colors[i+j*3] = color.r; colors[i+j*3+1] = color.g; colors[i+j*3+2] = color.b;}
+
+        // Positions H1
+        positions[i+9]  = ax; positions[i+10] = ay; positions[i+11] = az;
+        positions[i+12] = normal ? cx : dx; positions[i+13] = normal ? cy : dy; positions[i+14] = normal ? cz : dz;
+        positions[i+15] = normal ? dx : cx; positions[i+16] = normal ? dy : cy; positions[i+17] = normal ? dz : cz;
+
+        // Normals H2
+        pA.set(ax, ay, az);
+        normal ? pB.set(cx, cy, cz) : pB.set(dx, dy, dz);
+        normal ? pC.set(dx, dy, dz) : pC.set(cx, cy, cz);
+        cb.subVectors(pC, pB);
+        ab.subVectors(pA, pB);
+        cb.cross(ab);
+        cb.normalize();
+        nx = cb.x; ny = cb.y; nz = cb.z;
+        for (j = 0; j<3; ++j) {normals[i+9+3*j]   = nx; normals[i+9+3*j+1] = ny; normals[i+9+3*j+2] = nz;}
+
+        // Colors H2
+        color.setRGB((ax/n)+0.5, (ay/n)+0.5, (az/n)+0.5);
+        for (j = 0; j<3; ++j) {colors[i+9+j*3] = color.r; colors[i+9+j*3+1] = color.g; colors[i+9+j*3+2] = color.b;}
+
+    } else if (faceId < 2 * ijS) {
+        faceId -= ijS;
+        ii = faceId % iS;
+        jj = (faceId - ii) / iS;
+        kk = (faceId - ii - jj*iS) / ijS;
+
+        ax = iChunkOffset + ii; ay = jChunkOffset + 1 + jj; az = kk;
+        bx = ax + 1; by = ay; bz = az;
+        cx = ax + 1; cy = ay; cz = az;
+        dx = ax; dy = ay; dz = az + 1;
+
+        positions[i]   = ax; positions[i+1] = ay; positions[i+2] = az;
+        positions[i+3] = normal ? bx : cx; positions[i+4] = normal ? by : cy; positions[i+5] = normal ? bz : cz;
+        positions[i+6] = normal ? cx : bx; positions[i+7] = normal ? cy : by; positions[i+8] = normal ? cz : bz;
+
+        // Normals H1
+        pA.set(ax, ay, az);
+        normal ? pB.set(bx, by, bz) : pB.set(cx, cy, cz);
+        normal ? pC.set(cx, cy, cz) : pC.set(bx, by, bz);
+        cb.subVectors(pC, pB);
+        ab.subVectors(pA, pB);
+        cb.cross(ab);
+        cb.normalize();
+        nx = cb.x; ny = cb.y; nz = cb.z;
+        for (j = 0; j<3; ++j) {normals[i+3*j]   = nx; normals[i+3*j+1] = ny; normals[i+3*j+2] = nz;}
+
+        // Colors H1
+        color.setRGB((ax/n)+0.5, (ay/n)+0.5, (az/n)+0.5);
+        for (j = 0; j<3; ++j) {colors[i+j*3] = color.r; colors[i+j*3+1] = color.g; colors[i+j*3+2] = color.b;}
+
+        // Positions H2
+        positions[i+9]  = ax; positions[i+10] = ay; positions[i+11] = az;
+        positions[i+12] = normal ? cx : dx; positions[i+13] = normal ? cy : dy; positions[i+14] = normal ? cz : dz;
+        positions[i+15] = normal ? dx : cx; positions[i+16] = normal ? dy : cy; positions[i+17] = normal ? dz : cz;
+
+        // Normals H2
+        pA.set(ax, ay, az);
+        normal ? pB.set(cx, cy, cz) : pB.set(dx, dy, dz);
+        normal ? pC.set(dx, dy, dz) : pC.set(cx, cy, cz);
+        cb.subVectors(pC, pB);
+        ab.subVectors(pA, pB);
+        cb.cross(ab);
+        cb.normalize();
+        nx = cb.x; ny = cb.y; nz = cb.z;
+        for (j = 0; j<3; ++j) {normals[i+9+3*j]   = nx; normals[i+9+3*j+1] = ny; normals[i+9+3*j+2] = nz;}
+
+        // Colors H2
+        color.setRGB((ax/n)+0.5, (ay/n)+0.5, (az/n)+0.5);
+        for (j = 0; j<3; ++j) {colors[i+9+j*3] = color.r; colors[i+9+j*3+1] = color.g; colors[i+9+j*3+2] = color.b;}
+
+    } else {
+        faceId -= (2 * ijkS);
+        ii = faceId % iS;
+        jj = ((faceId - ii) % ijS) / iS;
+        kk = Math.floor(faceId / ijS);
+
+        ax = iChunkOffset + ii; ay = jChunkOffset + jj; az = 1 + kk;
+        bx = ax; by = ay + 1; bz = az;
+        cx = ax + 1; cy = ay + 1; cz = az;
+        dx = ax + 1; dy = ay; dz = az;
+
+        // Positions H1
+        positions[i]   = ax; positions[i+1] = ay; positions[i+2] = az;
+        positions[i+3] = normal ? bx : cx; positions[i+4] = normal ? by : cy; positions[i+5] = normal ? bz : cz;
+        positions[i+6] = normal ? cx : bx; positions[i+7] = normal ? cy : by; positions[i+8] = normal ? cz : bz;
+
+        // Normals H1
+        pA.set(ax, ay, az);
+        normal ? pB.set(bx, by, bz) : pB.set(cx, cy, cz);
+        normal ? pC.set(cx, cy, cz) : pC.set(bx, by, bz);
+        cb.subVectors(pC, pB);
+        ab.subVectors(pA, pB);
+        cb.cross(ab);
+        cb.normalize();
+        nx = cb.x; ny = cb.y; nz = cb.z;
+        for (j = 0; j<3; ++j) {normals[i+3*j]   = nx; normals[i+3*j+1] = ny; normals[i+3*j+2] = nz;}
+
+        // Colors H1
+        color.setRGB((ax/n)+0.5, (ay/n)+0.5, (az/n)+0.5);
+        for (j = 0; j<3; ++j) {colors[i+j*3] = color.r; colors[i+j*3+1] = color.g; colors[i+j*3+2] = color.b;}
+
+        // Positions H2
+        positions[i+9]  = ax; positions[i+10] = ay; positions[i+11] = az;
+        positions[i+12] = normal ? cx : dx; positions[i+13] = normal ? cy : dy; positions[i+14] = normal ? cz : dz;
+        positions[i+15] = normal ? dx : cx; positions[i+16] = normal ? dy : cy; positions[i+17] = normal ? dz : cz;
+
+        // Normals H2
+        pA.set(ax, ay, az);
+        normal ? pB.set(cx, cy, cz) : pB.set(dx, dy, dz);
+        normal ? pC.set(dx, dy, dz) : pC.set(cx, cy, cz);
+        cb.subVectors(pC, pB);
+        ab.subVectors(pA, pB);
+        cb.cross(ab);
+        cb.normalize();
+        nx = cb.x; ny = cb.y; nz = cb.z;
+        for (j = 0; j<3; ++j) {normals[i+9+3*j]   = nx; normals[i+9+3*j+1] = ny; normals[i+9+3*j+2] = nz;}
+
+        // Colors H2
+        color.setRGB((ax/n)+0.5, (ay/n)+0.5, (az/n)+0.5);
+        for (j = 0; j<3; ++j) {colors[i+9+j*3] = color.r; colors[i+9+j*3+1] = color.g; colors[i+9+j*3+2] = color.b;}
+    }
+};
--- client/app/engine/graphics/graphics.sub.chunks.js
+++ client/app/engine/graphics/graphics.sub.chunks.js
@@ -40,7 +40,7 @@ App.Engine.Graphics.prototype.initChunk = function(chunkId, all) {
     var sunCapacity = Math.floor(3/2 * triangles);
     c.capacities = [sunCapacity];
     c.sizes = [triangles];
-    // TODO buffer resize optimization
+
     var positions = new Float32Array(sunCapacity * 3 * 3);
     var normals = new Float32Array(sunCapacity * 3 * 3);
     var colors = new Float32Array(sunCapacity * 3 * 3);
@@ -50,12 +50,8 @@ App.Engine.Graphics.prototype.initChunk = function(chunkId, all) {
     var pC = new THREE.Vector3();
     var cb = new THREE.Vector3();
     var ab = new THREE.Vector3();
-    var ax, bx, cx, dx, ay, by, cy, dy, az, bz, cz, dz;
-    var ii, jj, kk;
-    var nx, ny, nz;
     var n = 800;
     var color = new THREE.Color();
-    var j;
 
     var i = 0;
     for (var f = 0; f < currentComponent.length; ++f) {
@@ -63,157 +59,12 @@ App.Engine.Graphics.prototype.initChunk = function(chunkId, all) {
         c.whereToFindFace[faceId] = [0, f]; // [In which geometry a given face is, at which position]
         c.whichFaceIs[f] = faceId;
         var normal = faceId > 0;
-        if (faceId < ijS) {
-            ii = faceId % iS;
-            jj = (faceId - ii) / iS;
-            kk = (faceId - ii - jj*iS) / ijS;
 
-            ax = iChunkOffset + 1 + ii; ay = jChunkOffset + jj; az = kk;
-            bx = ax; by = ay; bz = az + 1;
-            cx = ax; cy = ay + 1; cz = az + 1;
-            dx = ax; dy = ay + 1; dz = az;
-
-            // Positions H1
-            positions[i]   = ax; positions[i+1] = ay; positions[i+2] = az;
-            positions[i+3] = normal ? bx : cx; positions[i+4] = normal ? by : cy; positions[i+5] = normal ? bz : cz;
-            positions[i+6] = normal ? cx : bx; positions[i+7] = normal ? cy : by; positions[i+8] = normal ? cz : bz;
-
-            // Normals H1
-            pA.set(ax, ay, az);
-            normal ? pB.set(bx, by, bz) : pB.set(cx, cy, cz);
-            normal ? pC.set(cx, cy, cz) : pC.set(bx, by, bz);
-            cb.subVectors(pC, pB);
-            ab.subVectors(pA, pB);
-            cb.cross(ab);
-            cb.normalize();
-            nx = cb.x; ny = cb.y; nz = cb.z;
-            for (j = 0; j<3; ++j) {normals[i+3*j]   = nx; normals[i+3*j+1] = ny; normals[i+3*j+2] = nz;}
-
-            // Colors H1
-            color.setRGB((ax/n)+0.5, (ay/n)+0.5, (az/n)+0.5);
-            for (j = 0; j<3; ++j) {colors[i+j*3] = color.r; colors[i+j*3+1] = color.g; colors[i+j*3+2] = color.b;}
-
-            // Positions H1
-            positions[i+9]  = ax; positions[i+10] = ay; positions[i+11] = az;
-            positions[i+12] = normal ? cx : dx; positions[i+13] = normal ? cy : dy; positions[i+14] = normal ? cz : dz;
-            positions[i+15] = normal ? dx : cx; positions[i+16] = normal ? dy : cy; positions[i+17] = normal ? dz : cz;
-
-            // Normals H2
-            pA.set(ax, ay, az);
-            normal ? pB.set(cx, cy, cz) : pB.set(dx, dy, dz);
-            normal ? pC.set(dx, dy, dz) : pC.set(cx, cy, cz);
-            cb.subVectors(pC, pB);
-            ab.subVectors(pA, pB);
-            cb.cross(ab);
-            cb.normalize();
-            nx = cb.x; ny = cb.y; nz = cb.z;
-            for (j = 0; j<3; ++j) {normals[i+9+3*j]   = nx; normals[i+9+3*j+1] = ny; normals[i+9+3*j+2] = nz;}
-
-            // Colors H2
-            color.setRGB((ax/n)+0.5, (ay/n)+0.5, (az/n)+0.5);
-            for (j = 0; j<3; ++j) {colors[i+9+j*3] = color.r; colors[i+9+j*3+1] = color.g; colors[i+9+j*3+2] = color.b;}
-
-        } else if (faceId < 2 * ijS) {
-            faceId -= ijS;
-            ii = faceId % iS;
-            jj = (faceId - ii) / iS;
-            kk = (faceId - ii - jj*iS) / ijS;
-
-            ax = iChunkOffset + ii; ay = jChunkOffset + 1 + jj; az = kk;
-            bx = ax + 1; by = ay; bz = az;
-            cx = ax + 1; cy = ay; cz = az;
-            dx = ax; dy = ay; dz = az + 1;
-
-            positions[i]   = ax; positions[i+1] = ay; positions[i+2] = az;
-            positions[i+3] = normal ? bx : cx; positions[i+4] = normal ? by : cy; positions[i+5] = normal ? bz : cz;
-            positions[i+6] = normal ? cx : bx; positions[i+7] = normal ? cy : by; positions[i+8] = normal ? cz : bz;
-
-            // Normals H1
-            pA.set(ax, ay, az);
-            normal ? pB.set(bx, by, bz) : pB.set(cx, cy, cz);
-            normal ? pC.set(cx, cy, cz) : pC.set(bx, by, bz);
-            cb.subVectors(pC, pB);
-            ab.subVectors(pA, pB);
-            cb.cross(ab);
-            cb.normalize();
-            nx = cb.x; ny = cb.y; nz = cb.z;
-            for (j = 0; j<3; ++j) {normals[i+3*j]   = nx; normals[i+3*j+1] = ny; normals[i+3*j+2] = nz;}
-
-            // Colors H1
-            color.setRGB((ax/n)+0.5, (ay/n)+0.5, (az/n)+0.5);
-            for (j = 0; j<3; ++j) {colors[i+j*3] = color.r; colors[i+j*3+1] = color.g; colors[i+j*3+2] = color.b;}
-
-            // Positions H2
-            positions[i+9]  = ax; positions[i+10] = ay; positions[i+11] = az;
-            positions[i+12] = normal ? cx : dx; positions[i+13] = normal ? cy : dy; positions[i+14] = normal ? cz : dz;
-            positions[i+15] = normal ? dx : cx; positions[i+16] = normal ? dy : cy; positions[i+17] = normal ? dz : cz;
-
-            // Normals H2
-            pA.set(ax, ay, az);
-            normal ? pB.set(cx, cy, cz) : pB.set(dx, dy, dz);
-            normal ? pC.set(dx, dy, dz) : pC.set(cx, cy, cz);
-            cb.subVectors(pC, pB);
-            ab.subVectors(pA, pB);
-            cb.cross(ab);
-            cb.normalize();
-            nx = cb.x; ny = cb.y; nz = cb.z;
-            for (j = 0; j<3; ++j) {normals[i+9+3*j]   = nx; normals[i+9+3*j+1] = ny; normals[i+9+3*j+2] = nz;}
-
-            // Colors H2
-            color.setRGB((ax/n)+0.5, (ay/n)+0.5, (az/n)+0.5);
-            for (j = 0; j<3; ++j) {colors[i+9+j*3] = color.r; colors[i+9+j*3+1] = color.g; colors[i+9+j*3+2] = color.b;}
-
-        } else {
-            faceId -= (2 * ijkS);
-            ii = faceId % iS;
-            jj = ((faceId - ii) % ijS) / iS;
-            kk = Math.floor(faceId / ijS);
-
-            ax = iChunkOffset + ii; ay = jChunkOffset + jj; az = 1 + kk;
-            bx = ax; by = ay + 1; bz = az;
-            cx = ax + 1; cy = ay + 1; cz = az;
-            dx = ax + 1; dy = ay; dz = az;
-
-            // Positions H1
-            positions[i]   = ax; positions[i+1] = ay; positions[i+2] = az;
-            positions[i+3] = normal ? bx : cx; positions[i+4] = normal ? by : cy; positions[i+5] = normal ? bz : cz;
-            positions[i+6] = normal ? cx : bx; positions[i+7] = normal ? cy : by; positions[i+8] = normal ? cz : bz;
-
-            // Normals H1
-            pA.set(ax, ay, az);
-            normal ? pB.set(bx, by, bz) : pB.set(cx, cy, cz);
-            normal ? pC.set(cx, cy, cz) : pC.set(bx, by, bz);
-            cb.subVectors(pC, pB);
-            ab.subVectors(pA, pB);
-            cb.cross(ab);
-            cb.normalize();
-            nx = cb.x; ny = cb.y; nz = cb.z;
-            for (j = 0; j<3; ++j) {normals[i+3*j]   = nx; normals[i+3*j+1] = ny; normals[i+3*j+2] = nz;}
-
-            // Colors H1
-            color.setRGB((ax/n)+0.5, (ay/n)+0.5, (az/n)+0.5);
-            for (j = 0; j<3; ++j) {colors[i+j*3] = color.r; colors[i+j*3+1] = color.g; colors[i+j*3+2] = color.b;}
-
-            // Positions H2
-            positions[i+9]  = ax; positions[i+10] = ay; positions[i+11] = az;
-            positions[i+12] = normal ? cx : dx; positions[i+13] = normal ? cy : dy; positions[i+14] = normal ? cz : dz;
-            positions[i+15] = normal ? dx : cx; positions[i+16] = normal ? dy : cy; positions[i+17] = normal ? dz : cz;
-
-            // Normals H2
-            pA.set(ax, ay, az);
-            normal ? pB.set(cx, cy, cz) : pB.set(dx, dy, dz);
-            normal ? pC.set(dx, dy, dz) : pC.set(cx, cy, cz);
-            cb.subVectors(pC, pB);
-            ab.subVectors(pA, pB);
-            cb.cross(ab);
-            cb.normalize();
-            nx = cb.x; ny = cb.y; nz = cb.z;
-            for (j = 0; j<3; ++j) {normals[i+9+3*j]   = nx; normals[i+9+3*j+1] = ny; normals[i+9+3*j+2] = nz;}
-
-            // Colors H2
-            color.setRGB((ax/n)+0.5, (ay/n)+0.5, (az/n)+0.5);
-            for (j = 0; j<3; ++j) {colors[i+9+j*3] = color.r; colors[i+9+j*3+1] = color.g; colors[i+9+j*3+2] = color.b;}
-        }
+        this.addFace(faceId, i, iS, ijS, ijkS,
+            positions, normals, colors, natures,
+            iChunkOffset, jChunkOffset,
+            pA, pB, pC, cb, ab,
+            normal, color, n);
 
         i += 18;
     }
@@ -312,15 +163,22 @@ App.Engine.Graphics.prototype.updateChunk = function(chunkId, components) {
         while (sizes[meshId] !== undefined && sizes[meshId] === capacities[meshId]) ++meshId;
         if (sizes[meshId] === undefined) {
             // Create geometry
-            geometries[meshId] = new THREE.Geometry();
-            materials[meshId] = new THREE.Material();
+            geometries[meshId] = new THREE.BufferGeometry();
+            materials[meshId] = new THREE.MeshPhongMaterial({
+                color: 0xaaaaaa, specular: 0xffffff, shininess: 250,
+                side: THREE.BackSide, vertexColors: THREE.VertexColors
+            });
             sizes[meshId] = 1;
             capacities[meshId] = 10;
-            meshes[meshId] = new THREE.Mesh(geometries[meshId], materials[meshId]);
             meshHasToBeAdded = true;
         }
 
-        // if (meshHasToBeAdded) this.scene.add(meshes[meshId]);
+        // TODO add other faces...
+
+        if (meshHasToBeAdded) {
+            meshes[meshId] = new THREE.Mesh(geometries[meshId], materials[meshId]);
+            this.scene.add(meshes[meshId]);
+        }
     }
 
     var updated = components[2];
